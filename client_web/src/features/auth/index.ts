32d8f68300f48ebd21850265f75f2456d4bc41ef
4lissa <alissa.gritti@epitech.eu>
2026-08-03 16:30:50 +0200

    feat: wire auth pages and providers

---
+++ client_web/src/features/auth/index.ts
@@ -0,0 +1,16 @@
+export { LoginForm } from "./components/LoginForm";
+export { RegisterForm } from "./components/RegisterForm";
+export {
+  useLogin,
+  useLogout,
+  useMe,
+  useRegister,
+  useUpdateProfile,
+} from "./hooks";
+export type {
+  LoginRequest,
+  RegisterRequest,
+  UpdateProfileRequest,
+  UserResponse,
+  LoginResponse,
+} from "./types";
