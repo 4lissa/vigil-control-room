1423ae5e7522dddb127b0c7208ad869b0b40a99f
4lissa <alissa.gritti@epitech.eu>
2026-08-17 00:59:19 +0200

    test: add releases hooks tests

--- client_web/src/mocks/handlers.ts
+++ client_web/src/mocks/handlers.ts
@@ -1,9 +1,11 @@
 import { authHandlers } from "./handlers/auth";
 import { teamsHandlers } from "./handlers/teams";
 import { incidentsHandlers } from "./handlers/incidents";
+import { releasesHandlers } from "./handlers/releases";
 
 export const handlers = [
   ...authHandlers,
   ...teamsHandlers,
   ...incidentsHandlers,
+  ...releasesHandlers,
 ];
